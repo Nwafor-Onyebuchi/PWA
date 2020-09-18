63335a91cde72371a8f7800e337340ae12ddee29
buchi <buchilazarus4@gmail.com>
2020-09-18 08:50:42 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import video from "./videos/pwa.mp4";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  // const getVideos = async () => {
+  //   try {
+  //     const res = await axios.get("http://orangevalleycaa.org/api/videos");
+  //     setData(res.data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+  // // console.log(data);
+  // useEffect(() => {
+  //   // getVideos();
+  // }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Art Videos</h1>
+
+        {/* {data.map((item) => (
+          <div key={item.id}>
+            <h2>{item.name}</h2>
+            <video src={item.video_url} controls height={200} />
+          </div>
+        ))} */}
       </header>
+      <div>
+        <video src={video} height={200} controls />
+      </div>
     </div>
   );
 }
