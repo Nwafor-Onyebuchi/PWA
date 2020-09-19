bb8631d24d49d21cdce65e3a2a842afabd3ff084
buchi <buchilazarus4@gmail.com>
2020-09-19 19:57:24 +0100

    Fix

--- src/App.js
+++ src/App.js
@@ -6,34 +6,30 @@ import video from "./videos/pwa.mp4";
 function App() {
   const [data, setData] = useState([]);
 
-  // const getVideos = async () => {
-  //   try {
-  //     const res = await axios.get("http://orangevalleycaa.org/api/videos");
-  //     setData(res.data);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
-  // // console.log(data);
-  // useEffect(() => {
-  //   // getVideos();
-  // }, []);
+  const getVideos = async () => {
+    try {
+      const res = await axios.get("http://orangevalleycaa.org/api/videos");
+      setData(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // console.log(data);
+  useEffect(() => {
+    getVideos();
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Art Videos</h1>
-
-        {/* {data.map((item) => (
-          <div key={item.id}>
-            <h2>{item.name}</h2>
-            <video src={item.video_url} controls height={200} />
-          </div>
-        ))} */}
       </header>
-      <div>
-        <video src={video} height={200} controls />
-      </div>
+      {data.map((item) => (
+        <div key={item.id}>
+          <h2>{item.name}</h2>
+          <video src={item.video_url} controls height={200} />
+        </div>
+      ))}
     </div>
   );
 }
